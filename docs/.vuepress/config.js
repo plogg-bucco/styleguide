15a681db01ee214d74214af849e13c5dbc78a4b2
elhaj abid <xanaghost@gmail.com>
2021-06-23 22:42:47 +0100

    chore: add custom config file

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  base: "/styleguide/",
+  lang: "en-US",
+  title: "Code Style",
+  description: "Bucco code style guide",
+
+  themeConfig: {
+    logo: "https://vuejs.org/images/logo.png"
+  }
+};
